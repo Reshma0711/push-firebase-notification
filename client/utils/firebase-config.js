5bf4a9dd4515e99b1a53585e44ddc4fcf2cae4c5
reshma0711 <reshmamudedla@gmail.com>
2025-04-30 17:05:31 +0530

    made change

--- client/utils/firebase-config.js
+++ client/utils/firebase-config.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyATxhB86fn_AaQeMm34YWLC1K74coGc89Q",
-  authDomain: "push-notification-45d4c.firebaseapp.com",
-  projectId: "push-notification-45d4c",
-  storageBucket: "push-notification-45d4c.firebasestorage.app",
-  messagingSenderId: "1094901488472",
-  appId: "1:1094901488472:web:679061889917d03822e32d",
-  measurementId: "G-B7RTT5FWHW",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyATxhB86fn_AaQeMm34YWLC1K74coGc89Q",
+//   authDomain: "push-notification-45d4c.firebaseapp.com",
+//   projectId: "push-notification-45d4c",
+//   storageBucket: "push-notification-45d4c.firebasestorage.app",
+//   messagingSenderId: "1094901488472",
+//   appId: "1:1094901488472:web:679061889917d03822e32d",
+//   measurementId: "G-B7RTT5FWHW",
+// };
 
 const vapidKey =
   "BOgiMBgJAz9MpXOZ1kn8GMdIJ1AKU6rGM8j9RNaRimz_4qRfCLmrRcjq9YXzAGzeBjM8da1pjjz5aaEW9NakfRc";
