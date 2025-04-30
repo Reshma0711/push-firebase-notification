e1b600b7278c77224c93100af81bed2f03c6d301
reshma0711 <reshmamudedla@gmail.com>
2025-04-30 17:09:12 +0530

    made changes in the code

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -5,13 +5,13 @@ importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
 
 
-// firebase.initializeApp({
-//   apiKey: "AIzaSyATxhB86fn_AaQeMm34YWLC1K74coGc89Q",
-//   authDomain: "push-notification-45d4c.firebaseapp.com",
-//   projectId: "push-notification-45d4c",
-//   messagingSenderId: "1094901488472",
-//   appId: "1:1094901488472:web:679061889917d03822e32d",
-// });
+firebase.initializeApp({
+  apiKey: "AIzaSyATxhB86fn_AaQeMm34YWLC1K74coGc89Q",
+  authDomain: "push-notification-45d4c.firebaseapp.com",
+  projectId: "push-notification-45d4c",
+  messagingSenderId: "1094901488472",
+  appId: "1:1094901488472:web:679061889917d03822e32d",
+});
 
 const messaging = firebase.messaging();
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,68 +4,42 @@ import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   const [fcmToken, setFcmToken] = useState(null);
-  // const [clicked, setClicked] = useState(false);
 
-  const fetchFcmToken = async () => {
-    if ("serviceWorker" in navigator && window.isSecureContext) {
-      try {
-        const token = await requestFCMToken();
-        setFcmToken(token);
-      } catch (err) {
-        console.error("FCM Token getting error:", err.message);
-      }
-    } else {
-      console.warn(
-        "Service workers are not supported or not in a secure context"
-      );
-    }
-  };
   useEffect(() => {
+    const fetchFcmToken = async () => {
+      if ("serviceWorker" in navigator && window.isSecureContext) {
+        try {
+          const token = await requestFCMToken();
+          setFcmToken(token);
+        } catch (err) {
+          console.error("FCM Token getting error:", err.message);
+        }
+      } else {
+        console.warn("Service workers are not supported or not in a secure context");
+      }
+    };
+
     fetchFcmToken();
   }, []);
 
-  // const handleClick = () => {
-  //   setClicked((prev) => !prev);
-  // };
-
   useEffect(() => {
-    const unsubscribe = onMessageListener()
-      .then((payload) => {
-        toast(
-          <div>
-            <strong>{payload.notification.title}</strong>
-            <br />
-            {payload.notification.body}
-          </div>,
-          { position: "top-right" }
-        );
-        console.log("Received Foreground Message", payload);
-      })
-      .catch((err) => console.log("error:", err));
-
-    return () => {
-      // Clean up logic here if needed (currently not necessary for `onMessage`)
-    };
+    onMessageListener((payload) => {
+      toast(
+        <div>
+          <strong>{payload.notification.title}</strong>
+          <br />
+          {payload.notification.body}
+        </div>,
+        { position: "top-right" }
+      );
+      console.log("Received Foreground Message", payload);
+    });
   }, []);
 
-  // onMessageListener()
-  //   .then((payload) => {
-  //     toast(
-  //       <div>
-  //         <strong>{payload.notification.title}</strong>
-  //         <strong>{payload.notification.body}</strong>
-  //       </div>,
-  //       { position: "top-right" }
-  //     );
-  //     console.log("Received Foreground Message", payload);
-  //   })
-  //   .catch((err) => console.log("error:", err));
-  // console.log("clicked", clicked);
   return (
     <div>
       <ToastContainer />
       <p>{fcmToken}</p>
-      {/* <button onClick={handleClick}>Enable</button> */}
     </div>
   );
 };
